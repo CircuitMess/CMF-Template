5f4acfdac10bfdf90c7442b1af953a638043c464
Fr0zen3k <jan.kresic@circuitmess.com>
2024-08-28 16:55:51 +0200

    Expanded class hash id to bigger size with inclusion of templated objects template arguments information, implemented support for templated objects and templated object casting, cleaned up main.cpp, implemented template types class for info regarding the types passed as template arguments

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,4 @@
 #include <Core/EntryPoint.h>
-#include "Misc/Enum.h"
 
 class TestApp : public Application {
 	GENERATED_BODY(TestApp, Application)
@@ -7,17 +6,14 @@ class TestApp : public Application {
 protected:
 	virtual void begin() noexcept override {
 		Super::begin();
-		TRACE_LOG("");
 	}
 
 	virtual void tick(float deltaTime) noexcept override {
 		Super::tick(deltaTime);
-		TRACE_LOG("");
 	}
 
 	virtual void onDestroy() noexcept override {
 		Super::onDestroy();
-		TRACE_LOG("");
 	}
 };
 
